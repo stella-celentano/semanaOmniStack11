ba8fe4c1341909508cc4e137f94848aae0ccc105
stella-celentano <stellacelentano99@gmail.com>
2020-03-27 20:12:21 -0300

    Front-end do app

--- aulas/frontend/src/index.js
+++ aulas/frontend/src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
+import ReactDOM from 'react-dom'; //DOM é a árvore de elementos
 import App from './App';
 
 
-ReactDOM.render(<App />, document.getElementById('root')
-);
-
+ReactDOM.render(<App />, document.getElementById('root'));
+//render coloca em tela o <App />, dentro da div com id root
